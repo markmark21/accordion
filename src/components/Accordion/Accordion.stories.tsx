76567fada22df9482c3c3e757a510bb4399cd3a2
Mark Khramov <hramov.mark321@gmail.com>
2023-05-17 16:23:03 +0300

    some stories

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -9,12 +9,32 @@ export default {
 };
 
 const callBack = action("accordion mode change fired");
+const onClickCallBack = action("some item was clicked");
 
-export const MenuCollapsedMode = () => <Accordion titleValue={"Menu"} collapsed={true} onChange={callBack}/>;
-export const UsersUnCollapsedMode = () => <Accordion titleValue={"Users"} collapsed={false} onChange={callBack}/>;
+export const MenuCollapsedMode = () => <Accordion titleValue={"Menu"} collapsed={true} onChange={callBack} items={[]} onClick={onClickCallBack}/>;
+export const UsersUnCollapsedMode = () => <Accordion titleValue={"Users"}
+                                                     collapsed={false}
+                                                     onChange={callBack}
+                                                     items={[
+                                                         {title: "Mark", value: 1},
+                                                         {title: "Alexey", value: 2},
+                                                         {title: "Valera", value: 3},
+                                                         {title: "Artem", value: 4}]}
+                                                     onClick={onClickCallBack}
+/>;
 
 export const ModeChanging = () => {
     const [value, setValue] = useState(true);
-    return <Accordion titleValue={"Users"} collapsed={value} onChange={() => setValue(!value)}/>
+    return <Accordion titleValue={"Users"}
+                      collapsed={value}
+                      onChange={() => setValue(!value)}
+                      items={[
+                          {title: "Mark", value: 1},
+                          {title: "Alexey", value: 2},
+                          {title: "Valera", value: 3},
+                          {title: "Artem", value: 4}
+                      ]}
+                      onClick={(id) => {alert(`user with ID ${id} should be happy`)}}
+    />;
 
-}
+};
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 
+export type ItemType = {
+    title: string,
+    value: any
+}
+
 type AccordionPropsType = {
     titleValue: string
     collapsed: boolean
     onChange: () => void;
+    items: ItemType[]
+    onClick: (value: any) => void;
 
 }
 
 
 const Accordion: React.FC<AccordionPropsType> = (props) => {
-    const {onChange, collapsed, titleValue} = props
+    const {onChange, collapsed, titleValue, items, onClick} = props;
     return (
         <div>
             <AccordionTitle title={titleValue}
                             onChange={onChange}
             />
-            {!collapsed && <AccordionBody/>}
+            {!collapsed && <AccordionBody items={items} onClick={onClick}/>}
         </div>
     );
 };
@@ -25,25 +32,29 @@ type AccordionTitlePropsType = {
     onChange: () => void;
 }
 
-const AccordionTitle: React.FC<AccordionTitlePropsType> =(props) => {
-    const {onChange, title} = props
+const AccordionTitle: React.FC<AccordionTitlePropsType> = (props) => {
+    const {onChange, title} = props;
     return <div>
-        <h3 style={{cursor: 'pointer'}} onClick={(e) => onChange()}>{title}</h3>
+        <h3 style={{cursor: "pointer"}} onClick={() => onChange()}>{title}</h3>
     </div>;
 
 
+};
+
+export type AccordionBodyPropsType = {
+    items: ItemType[]
+    onClick: (value: any) => void;
 }
 
-function AccordionBody() {
-    return (
-        <div>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </div>
-    );
-}
+const AccordionBody: React.FC<AccordionBodyPropsType> = (props) => {
+    const {items, onClick} = props;
+    return <ul>
+        {items.map((item, index) => (
+                <li key={index} onClick={() => {onClick(item.value)}}>{item.title}</li>
+        ))}
+    </ul>;
+
+
+};
 
 export default Accordion;
