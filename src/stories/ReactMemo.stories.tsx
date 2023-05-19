e6abca280c87a11d59ac14ef9d2ad0f2ebfff094
Mark Khramov <hramov.mark321@gmail.com>
2023-05-19 16:07:20 +0300

    React.memo

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+
+export default {
+    title: "React.memo demo",
+};
+
+
+const NewMessagesCounter = (props: any) => {
+    return <div>{props.count}</div>;
+};
+
+
+const UsersSecret = (props: { users: string[] }) => {
+    return <div>{
+        props.users.map((u, i) => <div key={i}>{u}</div>)
+    }</div>;
+};
+
+const Users = React.memo(UsersSecret)
+
+export const Example1 = () => {
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(["Mark", "Valera", "Artem"])
+    return (
+        <div>
+            <button onClick={()=> {setCounter(counter + 1)}}>+</button>
+            <NewMessagesCounter count={counter}/>
+            <Users users={users}/>
+        </div>
+    )
+
+};
