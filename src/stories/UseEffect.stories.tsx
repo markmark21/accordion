137d14babc3c2a70186bda399fefef6c48587de2
Mark Khramov <hramov.mark321@gmail.com>
2023-06-02 14:18:31 +0300

    setTimeout

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -28,7 +28,35 @@ export const SimpleExample = () => {
 
     return <>
         Hello, {counter} {fake}
-        <button onClick={() => {setFake(fake + 1)}}>fake+</button>
         <button onClick={() => {setCounter(counter + 1)}}>counter+</button>
+        <button onClick={() => {setFake(fake + 1)}}>fake+</button>
+    </>
+}
+
+export const SetTimeoutExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+
+    useEffect(() => {
+        console.log('useEffect every render');
+        document.title = counter.toString()
+    })
+
+    useEffect(() => {
+        console.log('useEffect only 1st render');
+        document.title = counter.toString()
+    }, [])
+
+    useEffect(() => {
+        console.log('useEffect 1st render and every counter change');
+        document.title = counter.toString()
+    }, [counter])
+
+
+
+    return <>
+        Hello, {counter} {fake}
+        <button onClick={() => {setCounter(counter + 1)}}>counter+</button>
+        <button onClick={() => {setFake(fake + 1)}}>fake+</button>
     </>
 }
